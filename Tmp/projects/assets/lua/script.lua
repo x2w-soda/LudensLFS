ba04c8ec1ddf814fa6b6327b6637380117955a0e
x2w <x2w.soda@gmail.com>
2025-10-06 16:43:55 -0500

    lua script demo assets

--- Tmp/projects/assets/lua/script.lua
+++ Tmp/projects/assets/lua/script.lua
@@ -1,39 +1,37 @@
 local ld = require 'ludens'
 
--- script that modifies a Mesh component
 local Script = {
-	time = 0.0,
-	origin = {},
-	radius = 5.0,
+	name = '',
+	rotSpeed = 540.0,
+	moveSpeed = 10.0
 }
 
 function Script:attach(comp)
-	io.write('script attached\n')
-	self.origin = comp.transform:get_position();
+	self.name = comp:get_name()
+	ld.debug.log('script attached to %s', self.name)
 end
 
 function Script:detach()
-	io.write('script detached\n')
+	ld.debug.log('script detached from %s', self.name)
 end
 
 function Script:update(comp, dt)
-	self.time = self.time + dt;
-	local rot = comp.transform:get_rotation();
-	local offsetX = math.sin(math.pi / 2.0 * self.time);
-	local offsetZ = math.cos(math.pi / 2.0 * self.time);
-
-	local rotSpeed = 180.0
-	rot.y = rot.y + rotSpeed * dt;
-	if rot.y > 360.0 then
-		rot.y = rot.y - 360.0;
+	local rot = comp.transform:get_rotation()
+	if ld.input.get_key('space') then
+		--ld.debug.log('L key pressed')
+		rot.y = rot.y + self.rotSpeed * dt
+		comp.transform:set_rotation(rot)
 	end
-	comp.transform:set_rotation(rot);
 
-	-- TODO: Vec3:copy()
-	-- local pos = { x=self.origin.x, y=self.origin.y, z=self.origin.z }
-	-- pos.x = pos.x + offsetX * self.radius;
-	-- pos.z = pos.z + offsetZ * self.radius;
-	-- comp.transform:set_position(pos);
+	local pos = comp.transform:get_position()
+	if ld.input.get_key('h') then
+		pos.x = pos.x + self.moveSpeed * dt
+		comp.transform:set_position(pos)
+	end
+	if ld.input.get_key('l') then
+		pos.x = pos.x - self.moveSpeed * dt
+		comp.transform:set_position(pos)
+	end
 end
 
 return Script
